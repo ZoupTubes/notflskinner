11a7109525f49e8c5db5f56ea83bffa8774f2af9
liquid <liquid@l1q.pw>
2020-10-01 13:23:14 -0500

    Not flskinner.

---
+++ flskinner/Bootstrap.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Newtonsoft.Json;
+
+namespace flskinner
+{
+    class Bootstrap
+    {
+        public static void Setup()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            var folderPath = string.Format(@"{0}\flskinner\", appDataPath);
+            var skinsFolderPath = string.Format(@"{0}\flskinner\skins\", appDataPath);
+
+            System.IO.Directory.CreateDirectory(folderPath);
+
+            var mainConfigPath = folderPath + "flskinner.json";
+            if (!File.Exists(mainConfigPath))
+            {
+                var stream = assembly.GetManifestResourceStream("flskinner.Default.flskinner.json");
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    File.WriteAllText(mainConfigPath, reader.ReadToEnd());
+                }
+            }
+
+            if (!Directory.Exists(skinsFolderPath))
+            {
+                Directory.CreateDirectory(skinsFolderPath);
+
+                var prefix = "flskinner.Default.skins.";
+
+                var resourceNames = Assembly.GetExecutingAssembly()
+                    .GetManifestResourceNames()
+                    .Where(name => name.StartsWith(prefix));
+
+                foreach (var name in resourceNames)
+                {
+                    var stream = assembly.GetManifestResourceStream(name);
+
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var path = string.Format("{0}{1}", skinsFolderPath, name.Replace(prefix, ""));
+                        File.WriteAllText(path, reader.ReadToEnd());
+                    }
+                }
+            }
+
+            try
+            {
+                Config.current = JsonConvert.DeserializeObject<Config>(Uncommentify(File.ReadAllText(mainConfigPath)));
+            }
+            catch (Exception e)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("An exception occured while loading the configuration file (flskinner.json in %appdata%/flskinner)");
+                sb.Append(e.Message);
+                MessageBox.Show(sb.ToString());
+                System.Environment.Exit(1);
+            }
+
+            // some users have it in the 64 bit program files :shrug:
+            if (!File.Exists(string.Format(@"{0}\FL64.exe", Config.current.flStudioPath)))
+            {
+                var x64Path = @"C:\Program Files\Image-Line\FL Studio 20\FL64.exe";
+                if (File.Exists(x64Path))
+                {
+                    Config.current.flStudioPath = x64Path;
+                    Config.current.Save();
+                }
+            }
+
+            if (!File.Exists(string.Format(@"{0}\FL64.exe", Config.current.flStudioPath)))
+            {
+                PickFLFolder();
+            }
+
+            Skin.skins = new List<Skin>();
+
+            foreach (var skinPath in Directory.GetFiles(skinsFolderPath))
+            {
+                try
+                {
+                    var skin = JsonConvert.DeserializeObject<Skin>(Uncommentify(File.ReadAllText(skinPath)));
+                    skin.fileName = Path.GetFileName(skinPath);
+                    Skin.skins.Add(skin);
+                }
+                catch (Exception e)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("An exception occured while loading the skin file ");
+                    sb.AppendLine("'" + Path.GetFileName(skinPath) + "'");
+                    sb.AppendLine("File is located under '" + Path.GetDirectoryName(skinPath) + "'");
+                    sb.Append(e.Message);
+                    MessageBox.Show(sb.ToString());
+                }
+            }
+        }
+
+        public static void PickFLFolder()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\";
+            dialog.IsFolderPicker = false;
+            dialog.Filters.Add(new CommonFileDialogFilter(@"FL64.exe", "*.exe"));
+            dialog.Title = "Please select your FL64.exe";
+
+            var res = dialog.ShowDialog();
+            if (res == CommonFileDialogResult.Ok)
+            {
+                var path = dialog.FileName;
+                if (Path.GetFileName(dialog.FileName) == "FL64.exe")
+                {
+                    Config.current.flStudioPath = Path.GetDirectoryName(dialog.FileName);
+                    Config.current.Save();
+                }
+                else
+                {
+                    MessageBox.Show("That is not FL64.exe!");
+
+                    PickFLFolder();
+                    return;
+                }
+            }
+            else if (res == CommonFileDialogResult.Cancel)
+            {
+                System.Environment.Exit(1);
+            }
+        }
+
+        private static string Uncommentify(string json)
+        {
+            // single line comments
+            json = Regex.Replace(json, @"\/\/.*", "");
+            // block comments
+            json = Regex.Replace(json, @"\/\*(\*(?!\/)|[^*])*\*\/", "");
+
+            return json;
+        }
+    }
+}
---
+++ flskinner/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace flskinner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Bootstrap.Setup();
+
+            foreach (var arg in Environment.GetCommandLineArgs())
+            {
+                if (arg == "-nogui")
+                {
+                    Core.inject(Config.current.flStudioPath, Config.current.flStudioPath + @"\FL64.exe");
+                    Environment.Exit(0);
+                }
+            }
+
+            InitializeComponent();
+
+            Version.Content = Core.Version;
+
+            flStudioPath.Content = Config.current.flStudioPath;
+
+            int selectedIndex = -1;
+
+            SkinsList.ItemsSource = Skin.skins;
+            SkinsList.SelectedIndex = selectedIndex;
+
+            UpdateSkinLabel();
+
+            setMixerColors.IsChecked = Config.current.setMixerColors;
+            setGridColors.IsChecked = Config.current.setGridColors;
+            setDefaultPatternColor.IsChecked = Config.current.setDefaultPatternColor;
+            setPlaylistTrackColors.IsChecked = Config.current.setPlaylistTrackColors;
+        }
+
+        private void UpdateSkinLabel()
+        {
+            foreach (Skin s in Skin.skins)
+            {
+                if (s.fileName == Config.current.currentSkin)
+                {
+                    currentSkin.Content = s.name;
+
+                    break;
+                }
+            }
+        }
+
+        private void LaunchFL_Click(object sender, RoutedEventArgs e)
+        {
+            Core.inject(Config.current.flStudioPath, Config.current.flStudioPath + @"\FL64.exe");
+        }
+
+        private void OpenSkinsFolder_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", Skin.GetSkinsFolder());
+        }
+
+        private void SkinsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems[0] != null)
+            {
+                Config.current.currentSkin = ((Skin)e.AddedItems[0]).fileName;
+                Config.current.Save();
+
+                UpdateSkinLabel();
+            }
+        }
+
+        private void ChangeFLFolder_Click(object sender, RoutedEventArgs e)
+        {
+            Bootstrap.PickFLFolder();
+            flStudioPath.Content = Config.current.flStudioPath;
+        }
+
+        private void OpenConfigFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var folderPath = string.Format(@"{0}\flskinner\", appDataPath);
+            Process.Start("explorer.exe", folderPath);
+        }
+
+        private void setMixerColors_Click(object sender, RoutedEventArgs e)
+        {
+            Config.current.setMixerColors = ((CheckBox)sender).IsChecked.Value;
+            Config.current.Save();
+        }
+
+        private void setGridColorsFromSkin_Click(object sender, RoutedEventArgs e)
+        {
+            Config.current.setGridColors = ((CheckBox)sender).IsChecked.Value;
+            Config.current.Save();
+        }
+
+        private void setDefaultPatternColorFromSkin_Click(object sender, RoutedEventArgs e)
+        {
+            Config.current.setDefaultPatternColor = ((CheckBox)sender).IsChecked.Value;
+            Config.current.Save();
+        }
+
+        private void setPlaylistTrackColorsFromSkin_Click(object sender, RoutedEventArgs e)
+        {
+            Config.current.setPlaylistTrackColors = ((CheckBox)sender).IsChecked.Value;
+            Config.current.Save();
+        }
+
+        private void CreateShortcut(object sender, RoutedEventArgs e)
+        {
+            var desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var lnkName = "FL Studio (skinned)";
+
+            string lnkPath = System.IO.Path.Combine(desktopFolder, lnkName) + ".lnk";
+            System.IO.File.WriteAllBytes(lnkPath, new byte[] { });
+
+            Shell32.Shell shl = new Shell32.ShellClass();
+            Shell32.Folder dir = shl.NameSpace(desktopFolder);
+            Shell32.FolderItem itm = dir.Items().Item(lnkName + ".lnk");
+            Shell32.ShellLinkObject lnk = (Shell32.ShellLinkObject)itm.GetLink;
+
+
+            lnk.Path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            lnk.Arguments = "-nogui";
+            lnk.WorkingDirectory = System.IO.Path.GetDirectoryName(lnk.Path);
+            lnk.SetIconLocation(string.Format(@"{0}\FL64.exe", Config.current.flStudioPath), 0);
+
+            lnk.Save(lnkPath);
+        }
+    }
+}
